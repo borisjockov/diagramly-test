1a023317b53c8487cd4105e88bcc1561111171bd
Boris Jockov <boris.jockov@gmail.com>
2012-08-11 16:13:56 +0200

    del all

--- etc/sharejs/lib/client/web-prelude.js
+++
@@ -1,7 +0,0 @@
-var exports;
-
-window.sharejs = exports = {
-  'version': '0.5.0-pre'
-};
-
-if (typeof WEB === 'undefined') window.WEB = true;
--- etc/sharejs/lib/types/web-prelude.js
+++
@@ -1,9 +0,0 @@
-/**
-   @const
-   @type {boolean}
-*/
-var WEB = true;
-;
-var exports;
-
-exports = window['sharejs'];
